c708d9fee8faa5bef53f19a1f18257a523c3309c
Fortune Iheanacho <nachodev369@gmail.com>
2024-12-19 15:09:15 +0100

    Setting up Server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is runnng on PORT: ${PORT}`);
+});
