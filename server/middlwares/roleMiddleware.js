a8c8a8524f9ecc9a251ee8397db0202e58364091
Fortune Iheanacho <nachodev369@gmail.com>
2024-12-24 19:43:59 +0100

    Update: Made changes to Role Authorization for various types of users

--- server/middlwares/roleMiddleware.js
+++ server/middlwares/roleMiddleware.js
@@ -2,6 +2,7 @@ import User from '../models/userModel.js';
 
 export const roleMiddleware = (roles) => {
   const verifyRole = async (req, res, next) => {
+    // Check if the user is authenticated
     const user = await User.findOne({ email: userData.email });
     if (!user) {
       console.log(`User with email: ${userData.email} does not exists!`);
@@ -9,14 +10,17 @@ export const roleMiddleware = (roles) => {
         message: `User with email: ${userData.email} does not exists!`,
       });
     }
+    // Gets the user role from the user object
     const userRole = req.user.role;
 
+    // Check if the user role is in the roles array
     if (!roles.includes(userRole)) {
       return res
         .status(403)
         .json({ message: 'Access denied: insufficient permissions' });
     }
 
+    // Else, the user has the required role and can proceed to the next middleware
     next();
   };
 
--- server/middlwares/verifyJWTMiddleware.js
+++ server/middlwares/verifyJWTMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers['authorization'];
@@ -8,11 +9,21 @@ const verifyJWT = (req, res, next) => {
       .status(401)
       .json({ message: 'Unauthorized Access: TOKEN required!' });
   const access_token = authHeader.split(' ')[1];
-  jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: 'Invalid Token' });
-    req.user = decoded.email;
-    next();
-  });
+
+  // Verify the token (Might switch to try catch block)
+  jwt.verify(
+    access_token,
+    process.env.ACCESS_TOKEN_SECRET,
+    async (err, decoded) => {
+      if (err) return res.status(403).json({ message: 'Invalid Token' });
+      const user = await User.findOne({ email: decoded.email });
+
+      if (!user) return res.status(404).json({ message: 'User not found' });
+
+      req.user = user;
+      next();
+    }
+  );
 };
 
 export default verifyJWT;
