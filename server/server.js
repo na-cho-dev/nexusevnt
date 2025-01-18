abc6d501be8dd473eb90d7a7b49dd8e8a58ae9a8
Tai Keith <keithsteve.ks@hotmail.com>
2025-01-18 17:12:03 +0300

    Updated Client-Server linking

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import Stripe from 'stripe';
 import mongodb_connection from './database/mongoDBConnect.js';
 import authRouter from './routers/authRouter.js';
@@ -12,12 +15,26 @@ import verifyJWTMiddleware from './middlwares/verifyJWTMiddleware.js';
 import cookieParser from 'cookie-parser';
 import refreshTokenRouter from './routers/requestAccessTokenRouter.js';
 import paymentRouter from './routers/api/paymentRouter.js';
+import { handlePaymentSuccess } from './controllers/paymentController.js';  // Import handlePaymentSuccess
 
 const app = express();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 app.use(corsMiddleware);
+
+// Serve static files from the React build folder
+app.use(express.static(path.join(__dirname, '../client/nexus-evnt/build')));
+
+// Catch-all route to serve index.html for React's single-page app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/nexus-evnt/build', 'index.html'));
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
@@ -27,11 +44,10 @@ app.use(verifyJWTMiddleware);
 app.use('/', eventsRouter);
 app.use('/', ticketRouter);
 app.use('/account', attendeeRouter, organizerRouter);
-app.use('/api/create-payment-session', paymentRouter);
-// app.use('/payment-status/:sessionId', getPaymentStatus);
+app.use('/api', paymentRouter);
 
+// Connect to the MongoDB database
 mongodb_connection();
-
 app.get('/', (req, res) => {
   res.json({ message: 'NexusEvnt Web Application!' });
 });
@@ -76,6 +92,7 @@ app.post(
         console.log(`Unhandled event type: ${event.type}`);
     }
 
+    // Respond to Stripe that the webhook was received successfully
     res.json({ received: true });
   }
 );
