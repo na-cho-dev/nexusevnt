2e17a32572fa06077f550619c28942c349d69589
Angie <angie.monnye@gmail.com>
2025-01-26 19:09:22 +0200

    Sign in and Sign up Update

--- client/nexus-evnt/src/pages/SignInPage.js
+++ client/nexus-evnt/src/pages/SignInPage.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import '../styles/SignInPage.css';
+import { useNavigate } from "react-router-dom";
+import {
+  Form,
+  Button,
+  Container,
+  Card,
+  Row,
+  Col,
+  InputGroup,
+} from "react-bootstrap";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Close from "../components/common/CloseButton";
 
 const SignIn = () => {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const [errorMessage, setErrorMessage] = useState("");
   const [success, setSuccess] = useState("");
@@ -19,47 +30,94 @@ const SignIn = () => {
 
     try {
       const response = await axios.post(
-        '/auth/login', // backend URL
+        "/auth/login", // backend URL
         { email, password },
         { withCredentials: true } // To include cookies in the request
       );
       const { accessToken } = response.data;
       console.log("Login successful:", accessToken);
-      // Save access token to local storage or context if needed
       setSuccess(response.data.message || "Logged In Successfully");
-      navigate('/HomePage'); // Redirect to the Home page after successful logging in
+      navigate("/HomePage"); // Redirect to the Home page
     } catch (error) {
-      console.error("Login failed:", error.response?.data?.message || error.message);
-      setErrorMessage(error.response?.data?.message || "An error occurred. Please try again.");
+      console.error(
+        "Login failed:",
+        error.response?.data?.message || error.message
+      );
+      setErrorMessage(
+        error.response?.data?.message || "An error occurred. Please try again."
+      );
     }
   };
 
   return (
-    <div className="sign-in-page">
-      <h1>Sign In</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <button type="submit">Sign In</button>
-      </form>
-    </div>
+    <Container className="mt-5">
+      <div className="p-4 shadow">
+        <Card.Body>
+          <Row
+            className="shadow-lg p-4 rounded w-100"
+            style={{ background: "white" }}
+          >
+            <Col
+              md={6}
+              className="d-none d-md-flex flex-column justify-content-center text-white px-4"
+              style={{ background: "#1D1B31" }}
+            >
+              <h3>Discover tailored events.</h3>
+              <p>Sign in for personalized recommendations today!</p>
+            </Col>
+            <Col md={6} className="p-4">
+              <div
+                className="form-container mx-auto"
+                style={{ maxWidth: "500px" }}
+              >
+                <Close />
+                <h3 className="text-center mb-4">Login</h3>
+
+                <Form onSubmit={handleLogin}>
+                  <Form.Group controlId="email" className="mb-3">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter your e-mail"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="password" className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <InputGroup>
+                      <Form.Control
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                      <Button
+                        variant="outline-secondary"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                      </Button>
+                    </InputGroup>
+                  </Form.Group>
+                  {errorMessage && (
+                    <p className="text-danger">{errorMessage}</p>
+                  )}
+                  <Button type="submit" variant="dark" className="w-100">
+                    Login
+                  </Button>
+                </Form>
+                <div className="text-center mt-3">
+                  Don't have an account? <a href="/SignUpPage">Sign up</a>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Card.Body>
+      </div>
+    </Container>
   );
 };
 
